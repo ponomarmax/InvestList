4139d5605208a582094ec2a742189c2674efd8a5
maksym.ponomarenko <ponomarenko.max2015@gmail.com>
2024-12-05 21:24:19 +0200

    fixed style

--- InvestList/Program.cs
+++ InvestList/Program.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Text;
 using Core.Entities;
 using Core.Interfaces;
 using DataAccess;
@@ -100,13 +101,67 @@ try
     app.UseMiddleware<WwwRedirectMiddleware>();
     app.Use(async (context, next) =>
     {
-        // context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
-        // context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        // context.Response.Headers.Add("Access-Control-Allow-Headers", "application/json");
-        context.Response.Headers.Add("Content-Security-Policy",
-            "default-src 'self'; script-src 'self' 'unsafe-inline' https://tpc.googlesyndication.com https://pagead2.googlesyndication.com https://code.jquery.com https://cdn.jsdelivr.net https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; font-src 'self' https://cdn.jsdelivr.net https://fonts.gstatic.com; img-src 'self' data: https://www.google-analytics.com https://pagead2.googlesyndication.com; frame-src 'self' https://www.google.com https://tpc.googlesyndication.com https://googleads.g.doubleclick.net https://pagead2.googlesyndication.com; object-src 'none'; connect-src 'self' https://www.google-analytics.com https://pagead2.googlesyndication.com;");
+        var csp = new StringBuilder();
+
+        // Default and script-src
+        csp.Append("default-src 'self'; ");
+        csp.Append("script-src 'self' 'unsafe-inline' ");
+
+        csp.Append("https://cdn.jsdelivr.net https://code.jquery.com ");
+        // Preview Mode
+        csp.Append("https://googletagmanager.com https://tagmanager.google.com ");
+        
+        // Google Analytic
+        csp.Append("https://*.googletagmanager.com ");
+        
+        // Google Ads
+        csp.Append("https://www.googleadservices.com https://www.google.com https://www.googletagmanager.com https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net ");
+        csp.Append(";");
+        
+        // Style src for Preview Mode
+        csp.Append($"style-src 'self' 'unsafe-inline' ");
+        csp.Append("https://cdn.jsdelivr.net ");
+        csp.Append("https://googletagmanager.com https://tagmanager.google.com https://fonts.googleapis.com ");
+        csp.Append(";");
+        
+        // Font src for Preview Mode
+        csp.Append("font-src 'self' ");
+        csp.Append("https://fonts.gstatic.com data: ");
+        csp.Append("https://cdn.jsdelivr.net");
+        
+        csp.Append(";");
+
+        // Image sources
+        csp.Append("img-src 'self' data: ");
+        // Preview Mode
+        csp.Append("https://googletagmanager.com https://tagmanager.google.com https://fonts.googleapis.com ");
+        // Google Analytic
+        csp.Append("https://*.google-analytics.com https://*.googletagmanager.com ");
+        csp.Append("https://*.analytics.google.com https://*.googletagmanager.com https://*.g.doubleclick.net https://*.google.com https://*.google.ua ");
+        // Google Ads
+        csp.Append("https://www.googletagmanager.com https://googleads.g.doubleclick.net https://www.google.com https://google.com https://www.google.com.ua https://pagead2.googlesyndication.com ");
+        csp.Append(";");
+        
+        // Connect sources
+        csp.Append("connect-src 'self' ");
+        // Google Analytic
+        csp.Append("https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com ");
+        csp.Append("https://*.g.doubleclick.net https://*.google.com https://*.google.com.ua ");
+        // Google Ads
+        csp.Append("https://pagead2.googlesyndication.com https://www.googleadservices.com https://www.google.com https://google.com ");
+        csp.Append(";");
+
+        // Frame sources
+        csp.Append("frame-src 'self' https://www.googletagmanager.com ");
+        csp.Append("https://td.doubleclick.net https://www.googletagmanager.com ");
+        csp.Append("https://googleads.g.doubleclick.net ");
+        csp.Append(";");
+
+        // Finalize the Content Security Policy
+        context.Response.Headers.Add("Content-Security-Policy", csp.ToString());
         await next();
     });
+
     if (app.Environment.IsDevelopment())
     {
         app.UseMigrationsEndPoint();

cd2b7b0f95fc2e47a59f7f99361650e90543632d
maksym.ponomarenko <ponomarenko.max2015@gmail.com>
2025-03-04 09:13:53 +0200

    feat: added migrations

---
+++ DataAccess/Migrations/20250304064536_SaveRequestInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SaveRequestInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "FailedToSendEmailVerification",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "UserRequestInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IpAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserAgent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cookies = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Headers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RequestTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRequestInfos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserRequestInfos");
+
+            migrationBuilder.DropColumn(
+                name: "FailedToSendEmailVerification",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ DataAccess/Migrations/20250304071259_extend-info.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class extendinfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasChrome",
+                table: "UserRequestInfos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "MouseMoved",
+                table: "UserRequestInfos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "NavigatorWebdriver",
+                table: "UserRequestInfos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ScreenHeight",
+                table: "UserRequestInfos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ScreenWidth",
+                table: "UserRequestInfos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TimeSpent",
+                table: "UserRequestInfos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasChrome",
+                table: "UserRequestInfos");
+
+            migrationBuilder.DropColumn(
+                name: "MouseMoved",
+                table: "UserRequestInfos");
+
+            migrationBuilder.DropColumn(
+                name: "NavigatorWebdriver",
+                table: "UserRequestInfos");
+
+            migrationBuilder.DropColumn(
+                name: "ScreenHeight",
+                table: "UserRequestInfos");
+
+            migrationBuilder.DropColumn(
+                name: "ScreenWidth",
+                table: "UserRequestInfos");
+
+            migrationBuilder.DropColumn(
+                name: "TimeSpent",
+                table: "UserRequestInfos");
+        }
+    }
+}
